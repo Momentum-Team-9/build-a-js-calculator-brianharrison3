b0b47d802acaab1e9d6331e4a5999e036e2a95cd
Brian Harrison <brianharrison.2002@gmail.com>
2021-07-15 10:10:33 -0400

    buttons work, need to save first number

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 const num1 = document.getElementById('num1')
+const num2 = document.getElementById('num2')
 const display = document.getElementById('display')
-const firstOperand = null
 
 
-document.addEventListener('click', function(event){
+
+num1.addEventListener('click', function(event){
+  
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+num2.addEventListener('click', function(event){
+
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+
+})
+
+num3.addEventListener('click', function(event){
 
     console.log(event.target.innerText)
     console.log('button clicked')
@@ -12,6 +31,111 @@ document.addEventListener('click', function(event){
     
 
 })
+
+num4.addEventListener('click', function(event){
+
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
+
+num5.addEventListener('click', function(event){
+
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
+
+num6.addEventListener('click', function(event){
+
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
+
+num7.addEventListener('click', function(event){
+
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
+
+num8.addEventListener('click', function(event){
+
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
+
+num9.addEventListener('click', function(event){
+
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
+
+plus.addEventListener('click', function(event){
+    console.log(display.innerText);
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
+minus.addEventListener('click', function(event){
+    console.log(display.innerText);
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+times.addEventListener('click', function(event){
+    console.log(display.innerText);
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+divided.addEventListener('click', function(event){
+    console.log(display.innerText);
+    console.log(event.target.innerText)
+    console.log('button clicked')
+    display.innerText += event.target.innerText
+    console.log(event)
+    
+
+})
+
 
 
 
